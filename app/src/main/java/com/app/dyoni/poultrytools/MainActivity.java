872956320f5b7eede328813a26613cc01094b40e
Dyonie <dyonie.fine@gmail.com>
2015-07-17 17:24:48 +0700

    Initial commit

---
+++ app/src/main/java/com/app/dyoni/poultrytools/MainActivity.java
@@ -0,0 +1,124 @@
+package com.app.dyoni.poultrytools;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.cengalabs.flatui.FlatUI;
+import com.cengalabs.flatui.views.FlatButton;
+import com.cengalabs.flatui.views.FlatEditText;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+    private ArrayList<FlatEditText> flatEditTexts = new ArrayList<FlatEditText>();
+    private ArrayList<FlatButton> flatButtons = new ArrayList<FlatButton>();
+    private ArrayList<String> inputPakanString = new ArrayList<>();
+    private ArrayList<Float> inputPakanFloat = new ArrayList<Float>();
+    private ArrayList<Float> hasilProtein = new ArrayList<Float>();
+    float sum= (float) 0.0;
+    float protein_pakanJadi=(float) 0.22;
+    float protein_bekatulPadi=(float) 0.13;
+    float protein_dedakPadi=(float) 0.10;
+    float protein_jagungGiling=(float) 0.09;
+    float protein_polar=(float) 0.16;
+    float protein_tepungIkanKls1=(float) 0.60;
+    float protein_tepungIkanKls2=(float) 0.40;
+    float protein_pakankonsentrat=(float) 0.32;
+    float protein_bungkilKedele=(float) 0.44;
+    float protein_minyakSawit=(float) 0.0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FlatUI.initDefaultValues(this);
+        FlatUI.setDefaultTheme(FlatUI.DEEP);//set the theme
+        getSupportActionBar().setBackgroundDrawable(FlatUI.getActionBarDrawable(this, FlatUI.GRASS, false, 2));
+
+        flatEditTexts.add((FlatEditText) findViewById(R.id.edittext_pakanJadi));
+        flatEditTexts.add((FlatEditText) findViewById(R.id.edittext_bekatulPadi));
+        flatEditTexts.add((FlatEditText) findViewById(R.id.edittext_dedakPadi));
+        flatEditTexts.add((FlatEditText) findViewById(R.id.edittext_jagungGiling));
+        flatEditTexts.add((FlatEditText) findViewById(R.id.edittext_polar));
+        flatEditTexts.add((FlatEditText) findViewById(R.id.edittext_tepungIkanKls1));
+        flatEditTexts.add((FlatEditText) findViewById(R.id.edittext_tepungIkanKls2));
+        flatEditTexts.add((FlatEditText) findViewById(R.id.edittext_konsentrat));
+        flatEditTexts.add((FlatEditText) findViewById(R.id.edittext_bungkilKedele));
+        flatEditTexts.add((FlatEditText) findViewById(R.id.edittext_minyakSawit));
+
+        flatButtons.add((FlatButton) findViewById(R.id.button_hitung));
+        flatButtons.get(0).setOnClickListener(this);
+
+
+
+    }
+
+void getAllInputs(){
+    for(int i=0;i<10;i++) {
+        if((flatEditTexts.get(i).getText().toString()).isEmpty()){
+            inputPakanString.add("0");
+        }else{
+        inputPakanString.add(flatEditTexts.get(i).getText().toString());
+    }
+    }
+   // for(int i=0;i<10;i++) {
+     //   Log.d("inputs",inputPakanString.get(i));
+    //}
+
+}
+
+void convertToFloatandSum(){
+    for(int i=0;i<10;i++) {
+        inputPakanFloat.add((Float.parseFloat(inputPakanString.get(i))));
+    }
+    //for(int i=0;i<10;i++) {
+          // Log.d("inputs", String.valueOf(inputPakanFloat.get(i)));
+       // }
+
+    for(int i=0;i<10;i++) {
+        sum=sum+inputPakanFloat.get(i);
+    }
+ //   Log.d("sum", String.valueOf(sum));
+}
+
+   /* void calculateProtein(){
+        for(int i=0;i<10;i++) {
+            if((inputPakanFloat.get(i)).equals(0.0)){
+                hasilProtein.add(0.0)
+            }
+        }
+
+    }*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        getAllInputs();
+        convertToFloatandSum();
+    }
+}
