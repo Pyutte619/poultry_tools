882de4a6bdc50851b7f10de968bfce49034b0ef7
Dyonie <dyonie.fine@gmail.com>
2015-07-19 18:36:39 +0700

    Adding Edit Harga

---
+++ app/src/main/java/com/app/dyoni/poultrytools/SplashScreen.java
@@ -0,0 +1,38 @@
+package com.app.dyoni.poultrytools;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by piyut on 19/07/2015.
+ */
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
